6380c0af3cd54cd372bc70bac030d85e4dbb6f63
wracce <wracce@gmail.com>
2023-04-28 20:20:38 +0400

    initilize project

---
+++ bibliography-backend/src/main/java/com/wracce/bibliography/BibliographyApplication.java
@@ -0,0 +1,13 @@
+package com.wracce.bibliography;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliographyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliographyApplication.class, args);
+	}
+
+}
